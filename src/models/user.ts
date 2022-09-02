5f274defb34add8065b43335c178ebd0087a94ed
Elias Forte <eliasforte.dev@gmail.com>
2022-09-02 18:21:52 -0300

    refactor: models

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,5 +1,4 @@
-import mongoose from 'mongoose';
-import Model from '.';
+import mongoose, { Model } from 'mongoose';
 import { IUser } from '../utils/interfaces/IUser';
 
 const UserSchema = new mongoose.Schema<IUser>({
@@ -8,8 +7,34 @@ const UserSchema = new mongoose.Schema<IUser>({
   password: { type: String, required: true },
 }, { versionKey: false });
 
-export default class UserModel extends Model<IUser> {
-  constructor() {
-    super(mongoose.model('Users', UserSchema));
+export default class UserModel {
+  protected document: Model<IUser>;
+
+  constructor(schema: Model<IUser> = mongoose.model('Users', UserSchema)) {
+    this.document = schema;
+  }
+
+  public async create(infos: IUser): Promise<IUser> {
+    return this.document.create(infos);
+  }
+
+  public async findAll(): Promise<IUser[]> {
+    return this.document.find();
+  }
+
+  public async findById(id: string): Promise<IUser | null> {
+    return this.document.findById(id);
+  }
+
+  public async findByEmail(email: string): Promise<IUser | null> {
+    return this.document.findOne({ email });
+  }
+
+  public async update(id: string, infos: IUser): Promise<IUser | null> {
+    return this.document.findByIdAndUpdate(id, infos, { new: true });
+  }
+
+  public async delete(id: string): Promise<IUser | null> {
+    return this.document.findByIdAndDelete(id);
   }
 }
--- src/models/voucher.ts
+++ src/models/voucher.ts
@@ -1,5 +1,4 @@
-import mongoose from 'mongoose';
-import Model from '.';
+import mongoose, { Model } from 'mongoose';
 import { IVoucher } from '../utils/interfaces/IVoucher';
 
 const VoucherSchema = new mongoose.Schema<IVoucher>({
@@ -20,8 +19,34 @@ const VoucherSchema = new mongoose.Schema<IVoucher>({
   created_at: { type: Date, require: true },
 }, { versionKey: false });
 
-export default class VoucherModel extends Model<IVoucher> {
-  constructor() {
-    super(mongoose.model('Vouchers', VoucherSchema));
+export default class VoucherModel {
+  protected document: Model<IVoucher>;
+
+  constructor(schema: Model<IVoucher> = mongoose.model('Vouchers', VoucherSchema)) {
+    this.document = schema;
+  }
+
+  public async findAllByResponsible(id: string | undefined): Promise<IVoucher[]> {
+    return this.document.find({ responsible_id: id });
+  }
+
+  public async create(infos: IVoucher): Promise<IVoucher> {
+    return this.document.create(infos);
+  }
+
+  public async findAll(): Promise<IVoucher[]> {
+    return this.document.find();
+  }
+
+  public async findById(id: string): Promise<IVoucher | null> {
+    return this.document.findById(id);
+  }
+
+  public async update(id: string, infos: IVoucher): Promise<IVoucher | null> {
+    return this.document.findByIdAndUpdate(id, infos, { new: true });
+  }
+
+  public async delete(id: string): Promise<IVoucher | null> {
+    return this.document.findByIdAndDelete(id);
   }
 }
